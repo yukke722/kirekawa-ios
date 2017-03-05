d17eb63e95baa2f67b3b5b19151d88a10d99abff
hashi <hashimoto@5smile.com>
2017-03-05 22:44:17 +0700

    Create tables with fake data

---
+++ kirekawa/Util/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  kirekawa
+//
+//  Created by 橋本 祐介 on 2017/03/05.
+//  Copyright © 2017年 Yusuke Hashimoto. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let kKirekawa = "キレカワ"
+}
